1767f27139186f37d5ce666bdc8a62c32ea62b68
Nikita Grichan <anivire.work@gmail.com>
2024-06-27 22:15:46 +0300

    fix: auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -20,7 +20,8 @@ export default function Auth() {
 
       fetch(`${process.env.API_URL}access?accessToken=${accessToken}`, {
         method: 'POST',
-      }).then(async response => {
+        headers: { 'Content-Type': 'application/json' },
+      }).then(async (response) => {
         const data = await response.json();
         if (data.status === 'success') {
           console.log('Success');
@@ -35,7 +36,7 @@ export default function Auth() {
               setIsError(false);
               router.push('/');
             })
-            .catch(e => {
+            .catch((e) => {
               setIsError(true);
               setIsLoading(false);
               throw new Error(
@@ -56,25 +57,25 @@ export default function Auth() {
   return (
     <>
       <NextSeo
-        title="Get access"
-        description="Paste code below to access the site!"
+        title='Get access'
+        description='Paste code below to access the site!'
         noindex={true}
         nofollow={true}
       />
-      <section className="md:grid h-screen w-full md:grid-cols-3 items-center justify-end">
-        <div className="flex h-full w-full max-w-2xl flex-col items-center justify-center gap-5 bg-dot-body">
+      <section className='md:grid h-screen w-full md:grid-cols-3 items-center justify-end'>
+        <div className='flex h-full w-full max-w-2xl flex-col items-center justify-center gap-5 bg-dot-body'>
           <DotcreatorsLogoResponsive width={40} height={38} />
-          <p className="max-w-72 text-center text-zinc-400">
+          <p className='max-w-72 text-center text-zinc-400'>
             Welcome to dotcreators! Please paste code below to access the site
           </p>
-          <section className="h-15 flex flex-row items-center gap-5 rounded-2xl bg-dot-secondary p-4 px-5 outline-dot-primary focus-within:outline focus-within:outline-2 focus-within:outline-dot-rose">
+          <section className='h-15 flex flex-row items-center gap-5 rounded-2xl bg-dot-secondary p-4 px-5 outline-dot-primary focus-within:outline focus-within:outline-2 focus-within:outline-dot-rose'>
             <RiLockPasswordFill />
             <input
-              onChange={e => setAccessToken(e.target.value)}
-              type="password"
+              onChange={(e) => setAccessToken(e.target.value)}
+              type='password'
               maxLength={7}
-              placeholder="Input access token"
-              className="h-full bg-transparent outline-none placeholder:text-sm placeholder:text-zinc-400"
+              placeholder='Input access token'
+              className='h-full bg-transparent outline-none placeholder:text-sm placeholder:text-zinc-400'
             />
             <button
               onClick={() => {
@@ -84,22 +85,22 @@ export default function Auth() {
               {!isLoading ? (
                 <RiArrowRightLine />
               ) : (
-                <RiLoader5Fill className="animate-spin" />
+                <RiLoader5Fill className='animate-spin' />
               )}
             </button>
           </section>
           {isError && (
-            <p className="text-center text-sm text-dot-rose">Invalid code</p>
+            <p className='text-center text-sm text-dot-rose'>Invalid code</p>
           )}
         </div>
         <Image
-          src="/mesh-gradient.webp"
-          alt="Background"
+          src='/mesh-gradient.webp'
+          alt='Background'
           width={1000}
           height={500}
           quality={80}
           priority={true}
-          className="-z-10 md:block hidden col-span-2 h-screen w-full opacity-50 blur-md"
+          className='-z-10 md:block hidden col-span-2 h-screen w-full opacity-50 blur-md'
         />
       </section>
     </>
