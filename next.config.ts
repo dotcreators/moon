1874f08b5551b7b4ca171c9164a1e678dcbc249f
Nikita Grichan <anivire.work@gmail.com>
2025-03-06 20:29:13 +0300

    chore: add unocss icons

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+export default {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.twimg.com', port: '' },
+      { protocol: 'https', hostname: 'flagcdn.com', port: '' },
+    ],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+    UNAMI_ANALYTICS: process.env.UNAMI_ANALYTICS,
+    SITE_URL: process.env.SITE_URL,
+  },
 };
-
-export default nextConfig;
