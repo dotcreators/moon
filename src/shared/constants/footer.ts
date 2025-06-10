e76bb1e8537dc6c9396cfcf5c3dab08ab17c42f6
Nikita Grichan <anivire.work@gmail.com>
2025-06-10 23:45:32 +0300

    fix: label

---
+++ src/shared/constants/footer.ts
@@ -0,0 +1,44 @@
+const FOOTER = {
+  Support: [
+    {
+      chip: null,
+      label: 'Changelog',
+      link: '/changelog',
+      target: '_self',
+    },
+    {
+      chip: null,
+      label: 'Contact',
+      link: 'https://anivire.xyz',
+      target: '_blank',
+    },
+    {
+      chip: null,
+      label: 'Issues',
+      link: 'https://github.com/dotcreators/moon/issues',
+      target: '_blank',
+    },
+    {
+      chip: null,
+      label: 'Feedback',
+      link: 'https://github.com/orgs/dotcreators/discussions',
+      target: '_blank',
+    },
+  ],
+  Legal: [
+    {
+      chip: null,
+      label: 'Privacy Policy',
+      link: '/wiki/privacy',
+      target: '_self',
+    },
+    {
+      chip: null,
+      label: 'Data Gathering',
+      link: '/wiki/data-gathering',
+      target: '_self',
+    },
+  ],
+};
+
+export { FOOTER };
